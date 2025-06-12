7d3b5043523e5c9674a2a756963f5fcf0e669168
willyzen6 <54221946+willyzen6@users.noreply.github.com>
2025-06-12 17:37:55 -0500

    Cambios temporales en install and run.bat

--- install and run.bat
+++ install and run.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Batch file to set up the environment, install dependencies, and run Fooocus
-
+git pull
 REM Create a Python virtual environment if it does not exist
 if not exist "fooocus_env" (
     python -m venv fooocus_env
