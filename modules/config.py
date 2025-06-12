9619e7c7fbfd3f34260e447d98a43a63e9fe1f33
Future diffusion <54221946+futurediffusion@users.noreply.github.com>
2025-06-12 10:00:57 -0500

    Set default aspect ratio to 832x1216

--- modules/config.py
+++ modules/config.py
@@ -483,7 +483,7 @@ available_aspect_ratios = get_config_item_or_set_default(
 )
 default_aspect_ratio = get_config_item_or_set_default(
     key='default_aspect_ratio',
-    default_value='1152*896' if '1152*896' in available_aspect_ratios else available_aspect_ratios[0],
+    default_value='832*1216' if '832*1216' in available_aspect_ratios else available_aspect_ratios[0],
     validator=lambda x: x in available_aspect_ratios,
     expected_type=str
 )
