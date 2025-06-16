1637b31a9ab615050191cd9993ebde3187b8be07
Future diffusion <54221946+futurediffusion@users.noreply.github.com>
2025-06-16 16:21:46 -0500

    Fix default LoRA directory

--- modules/NewLoraSystem/sd_forge_lora/preload.py
+++ modules/NewLoraSystem/sd_forge_lora/preload.py
@@ -4,5 +4,11 @@ from modules.paths_internal import normalized_filepath
 
 
 def preload(parser):
-    parser.add_argument("--lora-dir", type=normalized_filepath, help="Path to directory with Lora networks.", default=os.path.join(paths.models_path, 'Lora'))
+    # Use the same default LoRA directory as Fooocus to keep compatibility
+    parser.add_argument(
+        "--lora-dir",
+        type=normalized_filepath,
+        help="Path to directory with Lora networks.",
+        default=os.path.join(paths.models_path, 'loras')
+    )
     parser.add_argument("--lyco-dir-backcompat", type=normalized_filepath, help="Path to directory with LyCORIS networks (for backawards compatibility; can also use --lyco-dir).", default=os.path.join(paths.models_path, 'LyCORIS'))
